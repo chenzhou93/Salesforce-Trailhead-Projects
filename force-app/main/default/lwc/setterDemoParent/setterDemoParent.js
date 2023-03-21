313e9f51717fcf1ebbf468fbcca7aeab6815d109
Chen Zhou <chenzhoux93@gmail.com>
2023-03-21 07:34:24 -0400

    setters

---
+++ force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class SetterDemoParent extends LightningElement {
+    userDetails = {
+        name: "salesforce troop",
+        age: 25
+    }
+}
